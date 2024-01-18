b8ed6609a6de8e0f346fb67043eebd5c3648c2ea
Sarper Makas <sarper.makas@superonline.com>
2024-01-18 22:38:07 +0300

    tree view improvement

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,27 @@ import java.io.File;
 
 public class Main {
     // ├ └ ─ │
+
+    private static String homePath = "/home/sarper/Documents/Programming/Python/Pygame";
+
     private static void listPaths(String pathname, String treeString) {
         File home = new File(pathname);
         File[] paths = home.listFiles();
   
         for (int i = 0; i < paths.length; i++) {
-            String spacing = new String(new char[treeString.length()]).replace('\0', ' ');
 
-            String start = (i ==paths.length-1) ? "└" : "├";
+            int length = paths[i].getAbsolutePath().substring((int)homePath.length()).replace('\\', '/').split("/").length;
 
-            System.out.println(spacing + start + "──" + paths[i].getName());
+            StringBuilder string = new StringBuilder();
+            string.append(new String(new char[length-2]).replace("\0", "   │")); // spacing
+            string.append("   ");
+            string.append((i ==paths.length-1) ? "└" : "├"); // start
+            string.append("──"); // tree
+
+
+            System.out.println(string + paths[i].getName());
+            
+            // move next directory
             if (paths[i].isDirectory()) {
                 listPaths(paths[i].toString(), treeString.toString() + "    ");
             }
@@ -19,6 +30,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        listPaths("/home/sarper/Documents/Programming/Python/Pygame", "");
+        listPaths(homePath, "");
     }
 }
